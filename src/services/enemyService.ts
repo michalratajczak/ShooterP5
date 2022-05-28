5f8410f1d6dc91730693f7821fd2a0467a77f89d
Michal Ratajczak <michalratajczak2105@gmail.com>
2022-05-28 12:48:22 +0200

    merge enemies

--- src/services/enemyService.ts
+++ src/services/enemyService.ts
@@ -5,17 +5,44 @@ import { Hero } from "../models/player/hero"
 
 const enemyService = () => {
   const spawnEnemy = (p5: P5, enemies: Unit[], hero: Hero) => {
-    if(p5.frameCount % 180 === 0) {
-      const x = p5.random(0, p5.windowWidth)
-      const y = p5.random(0, p5.windowHeight)
+    if(p5.frameCount % 60 === 0) {
+      const outerSide = p5.random(['x', 'y'])
+      let x: number, y: number
+      if (outerSide === 'x') {
+        x = p5.random(0, 1) > 0.5 ? p5.windowWidth + 100 : -100
+        y = p5.random(0, p5.windowHeight)
+      } else {
+        x = p5.random(0, p5.windowWidth)
+        y = p5.random(0, 1) > 0.5 ? p5.windowHeight + 100 : -100
+      }     
+
       enemies.push(new Zombie(p5, 1.5, 40, p5.createVector(x, y)))
     }
   }
 
   const moveEnemies = (enemies: Unit[], hero: Hero) => {
+    const enemiesToAbsorb = []
     for (const enemy of enemies) {
       const v = hero.position.copy().sub(enemy.position).normalize().mult(enemy.speed)
       enemy.position.add(v)
+
+      for (const other of enemies) {
+        if (enemy === other) continue
+        if (enemiesToAbsorb.some(e => e.absorbed === enemy)) continue
+        if (other.size > enemy.size) continue
+        if (enemy.position.dist(other.position) < enemy.size) {
+          enemiesToAbsorb.push({
+            grow: enemy,
+            absorbed: other
+          })                  
+        }
+      }      
+    }    
+
+    for (const x of enemiesToAbsorb) {
+      x.grow.size += 10
+      x.speed *= 0.95
+      enemies.splice(enemies.indexOf(x.absorbed), 1)
     }
   }
 
